fd470ca65523018efd70bae5fb02f9714dd579b1
Sai-Abbireddy <154813991+Sai-Abbireddy@users.noreply.github.com>
2024-03-29 17:37:18 +0530

    Add files via upload

---
+++ javascript/Core javascript 1.js
@@ -0,0 +1,22 @@
+/* 1  Write a pro,ram that ,rade' 'tudent' ba'ed on their mark'6
+& If grqatqr than 90 thqn A Gradp
+& If bqtwqqn 70 and 90 thqn a B gradp
+& If bqtwqqn  50 and 70 thqn a C gradp
+& Bqlow 50 thqn an F grade */
+
+marks=49
+if (marks>=90)
+{
+    console.log('A grade');
+}
+else if (marks>=70 && marks<90)
+{
+    console.log('B grade');
+}else if (marks>=50 && marks<70){
+    console.log('C grade');
+}else{
+    console.log('Fail');
+}
+
+
+
