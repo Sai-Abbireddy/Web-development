399b51db77db73c7a8f1557872169bb0018ec38e
Sai-Abbireddy <154813991+Sai-Abbireddy@users.noreply.github.com>
2024-04-03 11:40:46 +0530

    Add files via upload

--- 2.js
+++ 2.js
@@ -54,3 +54,159 @@ console.log('Max - Averege Difference:', maxDifference);
 
 
 
+
+//3
+
+
+
+const contactMap = new Map()
+
+//Adding contacts to the Map
+contactMap.set("John",{
+    age:30,
+    email: "john@example.com",
+    locatin: "Bangalore",
+});
+contactMap.set("Alice", {
+    age:25,
+    email:"alice@example.com",
+    locatin:"kolkata",
+});
+contactMap.set("Bob",{
+    age:35,
+    email:"bob@example.com",
+    location:"UP",
+});
+
+// Function to retrieve contact details based on name
+function getContact(name){
+   return contactMap.get(name);
+}
+
+console.log(getContact("John"));
+
+
+
+
+
+//4
+
+
+const person = { name:"Alice", age:25};
+const person = { name:"Bob', age:30};
+
+function introduce() {
+    console.log( 'Hello, I'm ${this.name}, and I'm ${this.age} years old.' );
+}
+introduce.call(person2);
+
+
+
+//5
+
+
+
+let uniqueNumbers = new set{[3,7,5,7,2,3,8]};
+numbers 
+
+let numbersquareMap = new Map();
+
+uniqueNumbers.forEach(number =>{
+    numberSquareMap.set(number,number*number);
+});
+
+console.log("Unique Numbers:");
+console.log(Array.from(uniqueNumbers).join(', '));
+
+console.log("\nNumber - square Map:");
+numberSquareMap.forEach(square, number)=>{
+    console.log('${number} : ${square}');
+});
+
+
+
+//6
+
+
+//Task 1: Create teh displayInfo function
+function displayInfo(name,role){
+    console.log('Name: ${name}, Role: ${role} ');
+}
+
+
+//Task 2: Use Call to invoke displayInfo with specific arguments
+displayInfo.call(null, "prabir", "Developer");
+
+
+//Task 3: Use apply to involve displayInfo with arguments as an array
+displayInfo.call(null,["son", "SDE"]);
+
+
+//Task 4: Create the greet function 
+function greet(){
+    console.log("Hello, ${ this name}!');
+}
+
+
+//Task 5: Use bind to create a new function with a specific context
+const user = {name:"PK"};
+const boundgreet = greet.bind(user);
+boundGreet();
+
+
+
+
+//7
+
+
+//Task 1: Create the calculator object
+const calculator = {
+   add:function(a,b) {
+    return a+b;
+   },
+   subtract: function(a,b) {
+    return a-b;
+   },
+   multiply: function(a,b) {
+    return a*b;
+   },
+   calculate: function (operation,a,b) {
+    if(operation === 'add'){
+       return this.add(a,b) ;
+    }else if(operation === 'subtract') {
+        return this.subtract(a,b);
+    }else if(operation === 'multiply') {
+        return this.multiply(a,b);
+    }
+   },
+};
+
+//Task 3: Use call to perform an addition operation
+const additionResult = calculator.calculator.call(calculator, 'add', 5,3);
+console.log('Addition Result: ${additionResult} ');
+
+
+//Task 4:Use apply to perform a multiplication operation with arguments as an array
+const multiplicationResult : calculator.calculate .apply(calculator,['multiply',4,2]);
+console.log('Multiplication Result: ${multiplicationResult}');
+Result : 8
+
+
+//Task 5:Create the discountCalculator object 
+const discountCalculator = {
+    discountpercentage:10,
+    applyDiscount:function (amount) {
+        return amount - (amount*this.discountpercentage)/100;
+    },
+};
+
+
+//Task 6:Use bind to create a new function bound to discountCalculator
+const calculatorDiscount =
+discountCalculator.applyDiscount.bind(discountCalculator);
+const discountAmount = calculateDiscount(100);
+console.log('Discount Amount: ${discountAmount} ');//Discounted Amount:90
+
+
+
+
